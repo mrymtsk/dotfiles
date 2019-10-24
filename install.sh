b69c4f91e7b82dec033f00b01eafe112b74fc8ab
Toshiki Murayama <murayama.toshiki@gmail.com>
2019-10-24 10:03:08 +0900

    edit install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 script_path=$(readlink -f "$0")
+script_name=$(basename "$script_path")
 PWD=$(dirname "$script_path")
 HOME=$(readlink -f "$1")
 
 DIRS_TO_LINK=$(find "$PWD" -path "$PWD/.git" -prune -o -name '*.link' -type d -print)
-FILES_TO_LINK=$(find "$PWD" -path "$PWD/.git" -prune -o -name '*.link' -type d -prune -o -name '*install.sh*' -type f -prune -o -type f -print)
+FILES_TO_LINK=$(find "$PWD" -path "$PWD/.git" -prune -o -name '*.link' -type d -prune -o -name "$script_name" -type f -prune -o -name '*~' -type f -prune -o -name '.gitignore' -type f -prune -o -name '.gitmodules' -type f -prune -o -name '*.swp' -type f -prune -o -type f -print)
 for source_dir in $DIRS_TO_LINK; do
     target_dir=${source_dir/$PWD/$HOME}
     target_dir=${target_dir%.link}
